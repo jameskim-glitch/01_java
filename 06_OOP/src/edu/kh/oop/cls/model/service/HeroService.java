b1c4254b2512a2c044fc68bcbfd84a3eaf9c7a71
jameskim-glitch <enxnwk@gmail.com>
2025-02-20 17:47:55 +0900

    히어로 연습

--- 06_OOP/src/edu/kh/oop/cls/model/service/HeroService.java
+++ 06_OOP/src/edu/kh/oop/cls/model/service/HeroService.java
@@ -3,17 +3,42 @@ package edu.kh.oop.cls.model.service;
 import edu.kh.oop.cls.model.vo.Hero;
 
 public class HeroService {
-	
-	public void practice() {
-		
-	
 
-	Hero h1 = new Hero("이구역짱", "전사", 200, 20, 1, 0); 
-	Hero h2 = new Hero("Zi존위자드", "마법사", 100, 100, 1, 0); 
-System.out.println(h1);
-System.out.println(h2);
+    public void practice() {
+
+        Hero h1 = new Hero("이구역짱", "전사", 200, 20, 1, 0);
+        Hero h2 = new Hero("Zi존위자드", "마법사", 100, 100, 1, 0);
+
+        System.out.println("===========캐릭터 생성=============");
+        System.out.println(h1);
+        System.out.println("===========캐릭터 생성=============");
+        System.out.println(h2);
+
+        System.out.println("===========이구역짱 시점=============");
+        h1.attack(100);
+        h1.attack(50.5);
+        h1.attack(49.5);
+        h1.dash();
+        h1.dash(); 
+
+        System.out.println("======='이구역짱'님의 정보=======");
+        System.out.println("- 현재 레벨 : " + h1.getLevel());
+        System.out.println("- 현재 hp : " + h1.getHp());
+        System.out.println("- 현재 mp : " + h1.getMp());
+        System.out.println("- 현재 exp: " + h1.getExp());
+
+
+        System.out.println("=========Zi존위자드 시점=============");
+        h2.attack(300);
+        h2.dash();
+        h2.attack(300);
+        h2.attack(300);
+
+        System.out.println("======='Zi존위자드'님의 정보=======");
+        System.out.println("- 현재 레벨: " + h2.getLevel());
+        System.out.println("- 현재 hp : " + h2.getHp());
+        System.out.println("- 현재 mp: " + h2.getMp());
+        System.out.println("- 현재 exp: " + h2.getExp());
+
+    }
 }
-	
-}
-
-
--- 06_OOP/src/edu/kh/oop/cls/model/vo/Hero.java
+++ 06_OOP/src/edu/kh/oop/cls/model/vo/Hero.java
@@ -1,83 +1,92 @@
 package edu.kh.oop.cls.model.vo;
 
 public class Hero {
-	private String nickname;
-	private String job;
-	private int hp;
-	private int mp;
-	private int level;
-	private double exp;
-	
-	public Hero(String nickname, String job , int hp,
-			int mp, int level,double exp) {
-		this.nickname = nickname;
-		this.job = job;
-		this.hp = hp;
-		this.mp = mp;
-		this.level = level;
-		this.exp = exp;
-	}
-	
-	public void attck(double exp) {
-		
-	}
-	
-	public void dash() {
-		
-	}
+    private String nickname;
+    private String job;
+    private int hp;
+    private int mp;
+    private int level;
+    private double exp;
 
-	@Override
-	public String toString() {
-		return "==========캐릭터생성=========\n" + job + "직업으로 " + nickname + "님이 생성되었습니다.\n" +"현재 레벨 :" 
-	+ level +"\n" + "현재 HP : " + hp +  "\n" + "현재 MP : " + mp + "\n" + "현재 경험치 : " + exp ;
-	}
+    public Hero(String nickname, String job, int hp, int mp, int level, double exp) {
+        this.nickname = nickname;
+        this.job = job;
+        this.hp = hp;
+        this.mp = mp;
+        this.level = level;
+        this.exp = exp;
+    }
 
-	public String getNickname() {
-		return nickname;
-	}
+    @Override
+    public String toString() {
+        return  job + " 직업으로 " + nickname + " 님이 생성되었습니다.\n" +
+                "현재 레벨 : " + level + "\n" + "현재 HP : " + hp + "\n" + "현재 MP : " + mp + "\n" +
+                "현재 경험치 : " + exp;
+    }
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
+    public String getNickname() {
+        return nickname;
+    }
 
-	public String getJob() {
-		return job;
-	}
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public void setJob(String job) {
-		this.job = job;
-	}
+    public String getJob() {
+        return job;
+    }
 
-	public int getHp() {
-		return hp;
-	}
+    public void setJob(String job) {
+        this.job = job;
+    }
 
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
+    public int getHp() {
+        return hp;
+    }
 
-	public int getMp() {
-		return mp;
-	}
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 
-	public void setMp(int mp) {
-		this.mp = mp;
-	}
+    public int getMp() {
+        return mp;
+    }
 
-	public int getLevel() {
-		return level;
-	}
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
 
-	public void setLevel(int level) {
-		this.level = level;
-	}
+    public int getLevel() {
+        return level;
+    }
 
-	public double getExp() {
-		return exp;
-	}
+    public void setLevel(int level) {
+        this.level = level;
+    }
 
-	public void setExp(double exp) {
-		this.exp = exp;
-	}
+    public double getExp() {
+        return exp;
+    }
 
+    public void setExp(double exp) {
+        this.exp = exp;
+    }
+
+    public void attack(double exp) {
+        this.exp += exp; 
+        System.out.println(nickname + "은/는 공격을 했다!!! 현재 경험치 : " + this.exp);
+        if (this.exp >= 300) {
+            level++;
+            System.out.println("레벨이 올랐습니다!! 현재레벨:" + level);
+        }
+    }
+
+    public void dash() {
+        if (mp >= 10) {
+            mp -= 10;
+            System.out.println(nickname + "의 엄청 빠른 대시!!! 남은 마력 : " + mp);
+        } else {
+            System.out.println("[마력 부족]더 이상 대시 할 수 없어요~!");
+        }
+    }
 }
