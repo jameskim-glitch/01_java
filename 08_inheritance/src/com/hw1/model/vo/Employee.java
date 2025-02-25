6b6532b0ee5d051ddab98e120cccb11daba29c49
jameskim-glitch <enxnwk@gmail.com>
2025-02-25 17:48:17 +0900

    상 속

---
+++ 08_inheritance/src/com/hw1/model/vo/Employee.java
@@ -0,0 +1,43 @@
+package com.hw1.model.vo;
+
+public class Employee extends Person {
+
+	private int salary;//급여
+	private String dept;//부서
+	 
+	public Employee() {}
+
+	
+	public Employee(String name, int age, double height, double weight, int salary, String dept) {
+		super(age, height, weight);
+		this.name = name; // 부모 person 에 작성된 필드중 protected 접근 제한자 name
+		this.salary = salary;
+		this.dept = dept;
+	}
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+	public String getDept() {
+		return dept;
+	}
+
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	@Override
+	public String information() {
+		return super.information() + String.format("/ 급여 : %d / 부서 : %s", salary, dept);
+	}
+
+	
+}
